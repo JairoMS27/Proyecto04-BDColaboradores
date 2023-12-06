0320998b7d189e6de1e939d86e194eca8b723a97
Jairo <jairomotasilva@icloud.com>
2023-12-06 03:04:11 +0100

    Add edit modal for collaborators

--- scripts/colaboradores.js
+++ scripts/colaboradores.js
@@ -157,37 +157,125 @@ function recargarTabla() {
       recargarTabla();
     });
   });
+
+  // Selecciona todos los botones de editar
+  var editButtons = document.querySelectorAll(".fa-user-pen");
+
+  // Obtiene el modal
+  var modal = document.querySelector("#editModal");
+
+  // Obtiene el botón de cerrar
+  var closeButton = document.querySelector(".modal .delete");
+
+  var id;
+
+  editButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      // Encuentra la fila de la tabla que contiene este botón
+      var row = button.parentElement.parentElement;
+
+      // Encuentra el ID del colaborador en esta fila
+      id = row.querySelector("td:first-child").textContent;
+
+      // Encuentra el colaborador correspondiente en el array de colaboradores
+      var colaborador = colaboradores.find(function (colaborador) {
+        return colaborador.id === id;
+      });
+
+      // Rellena el formulario de edición con los detalles del colaborador
+      document.querySelector("#editNombre").value = colaborador.nombre;
+      document.querySelector("#editTelefono").value = colaborador.telefono;
+      document.querySelector("#editEmail").value = colaborador.email;
+      document.querySelector("#editFechaIncorporacion").value =
+        colaborador.fechaIncorporacion;
+
+      // Establece la opción seleccionada en el elemento select para que coincida con el tipo del colaborador
+      document.querySelector("#editTipo").value = colaborador.tipo;
+
+      // Muestra el modal
+      modal.classList.add("is-active");
+    });
+  });
+
+  // Cuando se hace clic en el botón de cerrar, oculta el modal
+  closeButton.addEventListener("click", function () {
+    modal.classList.remove("is-active");
+  });
+
+  // Cuando se envía el formulario de edición
+  document
+    .getElementById("editForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      // Recoge los datos del formulario de edición
+      var nombre = document.querySelector("#editNombre").value;
+      var telefono = document.querySelector("#editTelefono").value;
+      var email = document.querySelector("#editEmail").value;
+      var fechaIncorporacion = document.querySelector(
+        "#editFechaIncorporacion"
+      ).value;
+      var tipo = document.querySelector("#editTipo").value;
+
+      // Encuentra el colaborador correspondiente en el array de colaboradores
+      var colaborador = colaboradores.find(function (colaborador) {
+        return colaborador.id === id;
+      });
+
+      // Actualiza los detalles del colaborador
+      colaborador.nombre = nombre;
+      colaborador.telefono = telefono;
+      colaborador.email = email;
+      colaborador.fechaIncorporacion = fechaIncorporacion;
+      colaborador.tipo = tipo;
+
+      // Guarda el array de colaboradores modificado
+      localStorage.setItem("colaboradores", JSON.stringify(colaboradores));
+
+      // Actualiza la tabla
+      recargarTabla();
+
+      // Oculta el modal
+      modal.classList.remove("is-active");
+    });
 }
 
 window.onload = function () {
   recargarTabla();
 };
 
-document.addEventListener('DOMContentLoaded', (event) => {
-  document.getElementById('searchButton').addEventListener('click', function() {
-    var input = document.getElementById('searchInput').value.toLowerCase();
-    
-    // Recuperar los colaboradores
-    var colaboradores = JSON.parse(localStorage.getItem("colaboradores"));
-  
-    // Filtrar los colaboradores
-    var colaboradoresFiltrados = colaboradores.filter(function(colaborador) {
-      return colaborador.nombre.toLowerCase().includes(input);
-    });
-  
-    // Resaltar las filas que coinciden
-    var rows = document.querySelector("table tbody").getElementsByTagName('tr');
-    for (var i = 0; i < rows.length; i++) {
-      var nameCell = rows[i].getElementsByTagName('td')[1]; // Asume que el nombre está en la segunda celda
-      if (nameCell) {
-        var name = nameCell.textContent || nameCell.innerText;
-        if (colaboradoresFiltrados.find(function(c) { return c.nombre.toLowerCase() === name.toLowerCase(); })) {
-          rows[i].style.backgroundColor = 'yellow'; // Cambia esto al color que prefieras
-        } else {
-          rows[i].style.backgroundColor = ''; // Restablece el color de fondo
+document.addEventListener("DOMContentLoaded", (event) => {
+  document
+    .getElementById("searchButton")
+    .addEventListener("click", function () {
+      var input = document.getElementById("searchInput").value.toLowerCase();
+
+      // Recuperar los colaboradores
+      var colaboradores = JSON.parse(localStorage.getItem("colaboradores"));
+
+      // Filtrar los colaboradores
+      var colaboradoresFiltrados = colaboradores.filter(function (colaborador) {
+        return colaborador.nombre.toLowerCase().includes(input);
+      });
+
+      // Resaltar las filas que coinciden
+      var rows = document
+        .querySelector("table tbody")
+        .getElementsByTagName("tr");
+      for (var i = 0; i < rows.length; i++) {
+        var nameCell = rows[i].getElementsByTagName("td")[1]; // Asume que el nombre está en la segunda celda
+        if (nameCell) {
+          var name = nameCell.textContent || nameCell.innerText;
+          if (
+            colaboradoresFiltrados.find(function (c) {
+              return c.nombre.toLowerCase() === name.toLowerCase();
+            })
+          ) {
+            rows[i].style.backgroundColor = "yellow"; // Cambia esto al color que prefieras
+          } else {
+            rows[i].style.backgroundColor = ""; // Restablece el color de fondo
+          }
         }
       }
-    }
-  });
+    });
 });
-
